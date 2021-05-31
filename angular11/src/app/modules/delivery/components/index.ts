cef0dcc299bfd4c0d3ac8d0722960d2166d0007a
David Mannies <dmannies@gmail.com>
2021-05-31 10:22:26 -0400

    results component added and Results interface added to service

--- angular11/src/app/modules/delivery/components/index.ts
+++ angular11/src/app/modules/delivery/components/index.ts
@@ -2,3 +2,5 @@ export * from './page-play/page-play.component';
 export * from './page-reports/page-reports.component';
 export * from './play/play.component';
 export * from './reports/reports.component';
+export * from './results/results.component';
+export * from './inputs/inputs.component';
