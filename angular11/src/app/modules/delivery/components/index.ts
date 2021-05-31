dc35b2ca7227433195be964f87f53d3e3b52f595
David Mannies <dmannies@gmail.com>
2021-05-31 00:38:01 -0400

    base components stubbed

--- angular11/src/app/modules/delivery/components/index.ts
+++ angular11/src/app/modules/delivery/components/index.ts
@@ -1,2 +1,4 @@
 export * from './page-play/page-play.component';
 export * from './page-reports/page-reports.component';
+export * from './play/play.component';
+export * from './reports/reports.component';
--- angular11/src/app/modules/delivery/delivery.module.ts
+++ angular11/src/app/modules/delivery/delivery.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PagePlayComponent, PageReportsComponent } from './components';
+import {
+  PagePlayComponent, PlayComponent,
+  PageReportsComponent, ReportsComponent,
+} from './components';
+import { InputsComponent } from './components/inputs/inputs.component';
 
 @NgModule({
   declarations: [
     PagePlayComponent,
-    PageReportsComponent
+    PageReportsComponent,
+    PlayComponent,
+    ReportsComponent,
+    InputsComponent
+  ],
+  exports: [
+    InputsComponent
   ],
   imports: [
     CommonModule
   ]
 })
 export class DeliveryModule { }
+
