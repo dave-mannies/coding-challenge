253fb0fdc3ced8f713e231a6ea296513cd67e606
David Mannies <dmannies@gmail.com>
2021-05-31 01:34:09 -0400

    play and reports show results

--- angular11/src/app/modules/delivery/components/reports/reports.component.ts
+++ angular11/src/app/modules/delivery/components/reports/reports.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DeliveryService, DeliveryResults } from "../../services";
+
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
@@ -7,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ReportsComponent implements OnInit {
+  results!: DeliveryResults;
 
-  constructor() { }
+  constructor(public dservice: DeliveryService) {
+    this.results = this.dservice.getDeliveryResults();
+
+    console.log(JSON.stringify(this.results, null, 2));
+  }
 
   ngOnInit(): void {
   }
