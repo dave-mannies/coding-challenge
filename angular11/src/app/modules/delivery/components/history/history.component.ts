8928e541b19ef8e64c82ba1440ef78f2cf103382
David Mannies <dmannies@gmail.com>
2021-06-01 01:59:50 -0400

    history view mode table

--- angular11/src/app/modules/delivery/components/history/history.component.ts
+++ angular11/src/app/modules/delivery/components/history/history.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { DeliveryService, Results } from "../../services";
+import { MatTableDataSource } from "@angular/material/table";
+import { DeliveryResults, DeliveryService, Results } from "../../services";
+
+export enum ViewEnum {
+  Cards,
+  Table
+}
 
 @Component({
   selector: 'app-history',
@@ -9,9 +15,14 @@ import { DeliveryService, Results } from "../../services";
 })
 export class HistoryComponent implements OnInit {
   results!: Results;
+  viewEnum = ViewEnum;
+  viewMode = ViewEnum.Cards;
+  dataSource = new MatTableDataSource<DeliveryResults>([]);
+  displayedColumns = ['id', 'date', 'deliverees', 'dispatch', 'delivered', 'houses'];
 
   constructor(public dservice: DeliveryService) {
     this.results = dservice.results;
+    this.dataSource.data = this.results.history;
   }
 
   ngOnInit(): void {
