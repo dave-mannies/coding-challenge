dfd181bdc431dcc72be74f03c25bfe1d388ee2a7
David Mannies <dmannies@gmail.com>
2021-06-03 18:43:12 -0400

    unittests refreshed

--- angular11/src/app/components/app/app.component.spec.ts
+++ angular11/src/app/components/app/app.component.spec.ts
@@ -39,6 +39,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.toolbar .title').textContent).not.toBeNull();
+    expect(compiled.querySelector('.toolbar .toolbar__title').textContent).not.toBeNull();
   });
 });
