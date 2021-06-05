c2966b38e6d1f5ffb3072c18d23ca06cb97deb39
David Mannies <dmannies@gmail.com>
2021-06-05 16:50:49 -0400

    reports now extends TrackingOptionsWrapper to handle filter settings

--- angular11/src/app/modules/delivery/components/reports/reports.component.ts
+++ angular11/src/app/modules/delivery/components/reports/reports.component.ts
@@ -4,7 +4,8 @@ import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 
 import { DeliveryResults, TrackingAnalysis } from "../../services";
-import { ExDeliveryEntry, TrackingOptions } from "../";
+import { ExDeliveryEntry } from "../play/play.component";
+import { TrackingOptions, TrackingOptionsWrapper } from "../tracking-options/tracking-options.component";
 
 @Component({
   selector: 'app-reports',
@@ -12,86 +13,38 @@ import { ExDeliveryEntry, TrackingOptions } from "../";
   styles: [
   ]
 })
-export class ReportsComponent implements OnInit {
+export class ReportsComponent extends TrackingOptionsWrapper implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  current?: DeliveryResults;
-  tracking: ExDeliveryEntry[] = [];
   dataSource = new MatTableDataSource<any>([]);
   displayedColumns = ['order', 'dId', 'x', 'y', 'pizzas'];
-  optionsDefault: TrackingOptions = {
-    deliverees: 0,
-    showDel: [true, true, true, true],
-    start: 0,
-    end: 0,
-    max: 0,
-    pmax: 0,
-    pizzas: 0
-  };
-  options: TrackingOptions = { ...this.optionsDefault };
 
   constructor() {
+    super();
   }
 
   ngOnInit(): void {
   }
 
-  navChanged(current: DeliveryResults) {
-    this.clear();
-
-    this.current = undefined;
-    this.tracking = [];
-
-    setTimeout(() => {
-      this.getTracking(current);
-    });
-  }
-
-  clear(): void {
-    this.options = { ...this.optionsDefault };
-    this.current = undefined;
-    this.tracking = [];
-  }
-
   getTracking(current: DeliveryResults): void {
-    this.current = current;
+    super.getTracking(current);
 
-    if (this.current && this.current.analysis.length) {
-      const analysis = this.current.analysis[0];
-      this.tracking = analysis.entries;
+    if (this.current) {
       this.dataSource.data = this.tracking;
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
-      this.dataSource.filterPredicate = (data: ExDeliveryEntry, filter: string): boolean => !data.hide ?? false;
-      this.analyze(analysis);
-      this.filter();
+      this.dataSource.filterPredicate =
+        (data: ExDeliveryEntry, filter: string): boolean => !data.hide ?? false;
     }
   }
 
-  analyze(analysis: TrackingAnalysis): void {
-    this.options.deliverees = this.current?.deliverees ?? 1;
-    this.options.max = Math.floor(this.tracking.length / (this.current?.deliverees ?? 1));
-    this.options.start = this.options.max > 0 ? 1 : 0;
-    this.options.end = this.options.max;
-    this.options.pmax = analysis.pmax;
-    this.options.pizzas = 1;
-  }
-
   tableFilter(): void {
-    this.dataSource.filter = 'fake'
+    this.dataSource.filter = 'trigger'
   }
 
   filter(): void {
-    this.tracking.forEach(track => {
-      track.hide =
-        this.options.start > track.order ||
-        this.options.end < track.order ||
-        track.pizzas < this.options.pizzas ||
-        !this.options.showDel[track.dId - 1];
-    });
-
+    super.filter();
     this.tableFilter();
   }
-
 }
