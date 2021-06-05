2c736b08606c613a0551cbd555503a7dd42c334d
David Mannies <dmannies@gmail.com>
2021-06-05 08:07:55 -0400

    history expanded stubbed

--- angular11/src/app/modules/delivery/components/history/history.component.ts
+++ angular11/src/app/modules/delivery/components/history/history.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from "@angular/material/table";
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
 import { DeliveryResults, DeliveryService, Results } from "../../services";
 
 export enum ViewEnum {
@@ -10,8 +12,36 @@ export enum ViewEnum {
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styles: [
-  ]
+  styles: [`
+    tr.example-detail-row {
+      height: 0;
+    }
+
+    tr.example-element-row:not(.example-expanded-row):hover {
+      background: whitesmoke;
+    }
+
+    tr.example-element-row:not(.example-expanded-row):active {
+      background: #efefef;
+    }
+
+    .example-element-row td {
+      border-bottom-width: 0;
+    }
+
+    .example-element-detail {
+      overflow: hidden;
+      display: flex;
+    }
+  `
+  ],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class HistoryComponent implements OnInit, AfterViewInit {
   results!: Results;
@@ -19,6 +49,7 @@ export class HistoryComponent implements OnInit, AfterViewInit {
   viewMode = ViewEnum.Cards;
   dataSource = new MatTableDataSource<DeliveryResults>([]);
   displayedColumns = ['id', 'date', 'deliverees', 'delivered', 'houses'];
+  expandedElement: any = null;
 
   constructor(public dservice: DeliveryService) {
     this.results = dservice.results;
