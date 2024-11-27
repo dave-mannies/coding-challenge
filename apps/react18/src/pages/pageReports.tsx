f3281ec1edba773efc4566667b3d09b5c039e72c
Dave Mannies <87336699+dave-koneksa@users.noreply.github.com>
2024-11-26 21:47:20 -0500

    reports page progress

--- apps/react18/src/pages/pageReports.tsx
+++ apps/react18/src/pages/pageReports.tsx
@@ -1,5 +1,27 @@
+import {useContext, useState} from 'react';
+import {DeliveryContext} from '../app/App';
+import ResultsHistory from '../components/results-history';
+import TrackingOptions from '../components/tracking-options';
+import EntriesTable from '../components/entriesTable';
+import {DeliveryResults, Results} from '../pizza-delivery/types';
+
 export default function PageReports() {
+  const deliveryService = useContext(DeliveryContext).deliveryService;
+  const [results, setResults] = useState<Results>(deliveryService.results);
+
+  const onSelected = (current: DeliveryResults, currentIndex: number) => {
+    const newResults = {...results, current, currentIndex};
+    setResults(newResults);
+    deliveryService.results = newResults;
+  };
+
   return (
-    <div>Reports</div>
+    <div className={'reports'}>
+      <ResultsHistory history={results.history} defaultIndex={results.currentIndex} onSelected={onSelected} />
+
+      <TrackingOptions />
+
+      <EntriesTable entries={results.current?.analysis?.[0].entries ?? []} />
+    </div>
   )
 }
